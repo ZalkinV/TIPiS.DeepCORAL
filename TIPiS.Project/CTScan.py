31488e53262307729f52591da4d335a1978df871
Zalkin <Zalkin.viktor@mail.ru>
2019-03-05 16:49:55 +0300

    Create class CTScan

---
+++ TIPiS.Project/CTScan.py
@@ -0,0 +1,12 @@
+import numpy as np
+import SimpleITK as sitk
+
+
+
+class CTScan:
+	def __init__(self, file_name, file_path = ""):
+		self.path = file_path + file_name + ".mhd"
+		self.sitk_image = sitk.ReadImage(self.path)
+		self.raw_image = sitk.GetArrayFromImage(self.sitk_image)
+		self.nodule = None
+		pass
