b26ee9acfd371cc6215c6f43ad23b7611f58efbf
Zalkin <Zalkin.viktor@mail.ru>
2019-03-16 11:04:14 +0300

    DP. Add delete_negative() inside prepare_candidates()

--- TIPiS.Project/data_processing.py
+++ TIPiS.Project/data_processing.py
@@ -16,10 +16,24 @@ def get_scan_names(scans_path):
 
 
 def prepare_candidates(file_name):
+
+	def delete_negative(candidates, neg_to_pos_ratio):
+		indexes_pos = np.array(candidates[candidates["class"] == 1].index)
+		indexes_neg = np.array(candidates[candidates["class"] == 0].index)
+
+		np.random.seed(104)
+		indexes_neg = np.random.choice(indexes_neg, len(indexes_pos) * neg_to_pos_ratio, replace=False)
+
+		indexes_all = np.concatenate((indexes_pos, indexes_neg))
+		resampled_candidates = candidates.iloc[indexes_all]
+		return resampled_candidates
+
+
 	candidates_all = pd.read_csv(file_name)
 	candidates_all.rename(columns={"coordX" : "x", "coordY" : "y", "coordZ" : "z"}, inplace=True)
-	candidates = candidates_all
-	return candidates
+	candidates_prep = delete_negative(candidates_all, 3)
+
+	return candidates_prep
 
 
 def cut_nodule(scan, world_coords, size):
