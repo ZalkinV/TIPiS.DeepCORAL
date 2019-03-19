a61ee101d4704a23993407669a88f52d14b32f7b
Zalkin <Zalkin.viktor@mail.ru>
2019-03-19 19:35:25 +0300

    TP. Remove unused imports as mentioned in #1

--- TIPiS.Project/TIPiS.Project.py
+++ TIPiS.Project/TIPiS.Project.py
@@ -1,13 +1,7 @@
-import pandas as pd
-import os
-
-from CTScan import CTScan
 import data_processing as dp
-import image_processing as im
 
 
 
 if __name__ == "__main__":
-	dp.process_data()
 	X_data, y_data = dp.load_scan_nodules()
 	pass
