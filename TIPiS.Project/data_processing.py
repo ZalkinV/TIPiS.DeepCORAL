eadf24c8889d8d080717f69ad3b157d1cf41f2d1
Nikita Belayev <NIkitaUniversity@yandex.ru>
2019-03-19 20:20:00 +0300

    DP. get_scan_names(). Better empty string checking
    Co-Authored-By: ZalkinV <32573127+ZalkinV@users.noreply.github.com>

--- TIPiS.Project/data_processing.py
+++ TIPiS.Project/data_processing.py
@@ -29,7 +29,7 @@ def get_scan_names(scans_path):
 	scan_names = set()
 	for file_name in os.listdir(scans_path):
 		file_name_wo_extension = file_name.rsplit('.', 1)[0]
-		if file_name_wo_extension != "":
+		if file_name_wo_extension:
 			scan_names.add(file_name_wo_extension)
 	return list(scan_names)
 
