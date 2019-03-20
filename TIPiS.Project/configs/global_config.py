c27db86bf73ff7157bdfd16b5903c4b4123feb3b
Nikita Belayev <NIkitaUniversity@yandex.ru>
2019-03-20 12:46:54 +0300

    #2 Вынести константы в файл config и подцепить к нему файл local_config (#2)
    #2 Вынести константы в файл config и подцепить к нему файл local_config

---
+++ TIPiS.Project/configs/global_config.py
@@ -0,0 +1,16 @@
+HU_MIN = -1000 # Min Haunsfield Unit
+HU_MAX = 400 # Max Haunsfield Unit
+SIZE_NODULE_IMAGE = 128
+
+
+# Roots
+PATH_SCANS = "../data/images_raw/"
+PATH_IMAGES_PREPARED = "../data/images_prepared/"
+PATH_ANNOTATION = "../data/info/annotations.csv"
+PATH_CANDIDATES = "../data/info/candidates.csv"
+
+
+try:
+    from local_config import *
+except ImportError:
+    pass
